79dc2e471045a2c20815dc13457570dadcc87548
savemeharry <135016441+savemeharry@users.noreply.github.com>
2025-04-21 22:54:43 +0500

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -100,5 +100,6 @@ def index():
     return 'Бот работает! Webhook активен.'
 
 # Для локальной отладки
-if __name__ == '__main__':
-    app.run(debug=True) 
+ if __name__ == '__main__':
+       port = int(os.environ.get('PORT', 8080))
+       app.run(host='0.0.0.0', port=port)
